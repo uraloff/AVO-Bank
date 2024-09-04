f504326f440b179abab113fffc70aea67201033e
Ali Uralov <uralovmuhammadali6110@gmail.com>
2024-09-04 22:14:38 +0500

    Добавлен логирование

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import logging
 import asyncio
 from os import getenv
 from dotenv import load_dotenv
+
 from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -10,20 +12,26 @@ from App.Handlers.uz_handlers import uz_user_router
 from App.Handlers.ru_handlers import ru_user_router
 
 
-async def on_startup():
-    print("Бот успешно запущен ✅")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s — %(name)s — %(levelname)s — %(message)s'
+)
 
 
 async def main():
     load_dotenv()
+
+    logging.info("Загрузка конфигурации...")
 
     bot = Bot(
         token=getenv('TOKEN'),
         default=DefaultBotProperties(parse_mode=ParseMode.HTML)
     )
     dp = Dispatcher()
-    dp.startup.register(on_startup)
     dp.include_routers(user_router, uz_user_router, ru_user_router)
+
+    logging.info("Бот успешно запущен ✅")
+
     await dp.start_polling(bot)
 
 
@@ -31,4 +39,4 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        print("Бот успешно выключен ❌")
+        logging.info("Бот успешно выключен ❌")
